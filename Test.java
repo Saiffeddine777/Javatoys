1688e5b9afd259f1f439617d449ed74cd5f0fec5
Saiffedddine777 <saiffeddinezouaghi@gmail.com>
2023-08-18 16:39:30 +0100

    more toys

--- Test.java
+++ Test.java
@@ -1,18 +1,14 @@
-import java.util.Stack;
 
+import java.util.ArrayList;
 
 public class Test{
 
     public static void main (String args[]){
-        Stack<Integer> array = new Stack<>();
-        array.push(1);
-        array.push(2);
-        array.push(3);
-        array.push(4);
-        System.out.println(array);
-
-        int[] numbers= new int[] {1,2,3,4};
-
-        System.out.println(numbers);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+     
+        
+        System.out.println(list.size());
     }
 }
